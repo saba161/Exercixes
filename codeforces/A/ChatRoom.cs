a580bc867506bfe7ee9b6aa8f63ca1abead0b3a7
kogo <saba.koguashvili1999@mail.ru>
2023-04-05 21:31:20 +0400

    new Task

---
+++ codeforces/A/ChatRoom.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace codeforce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string hello = "hello";
+            int count = 0;
+
+            for(int i = 0; i < n.Length; i++)
+            {
+                if(hello[count] == n[i])
+                {
+                    count++;
+
+                    if(count == 5)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if(count == 5)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
